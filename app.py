d79b0597a5dc85119a82711180e79c3031bf945b
wailingwailord <maximusburce@gmail.com>
2024-04-10 15:13:02 +0800

    fixed row deletion and editing

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from datetime import datetime
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-# db = SQLAlchemy(app)
 
 app.config['MYSQL_HOST'] = 'ccscloud.dlsu.edu.ph'
 app.config['MYSQL_PORT'] = 20201
@@ -15,27 +13,13 @@ app.config['MYSQL_DB'] = 'mco2'
 
 db = MySQL(app)
 
-# class Appointment(db.Model):
-#     pxid = db.Column(db.String(32))
-#     clinicid = db.Column(db.String(32))
-#     doctorid = db.Column(db.String(32))
-#     apptid = db.Column(db.String(32), primary_key=True)
-
-#     title = db.Column(db.String(200), nullable=False)
-#     date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))
-
-#     def __repr__(self):
-#         return '<Apt %r>' % self.apptid
-
-# stuff for db initialization
-# with app.app_context():
-#     db.create_all()
 
 def format_time(input):
     html_datetime = datetime.fromisoformat(input)
     mysql_datetime_str = html_datetime.strftime('%Y-%m-%d %H:%M:%S')
 
     return mysql_datetime_str
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -49,7 +33,6 @@ def index():
 
 @app.route('/add', methods=['POST', 'GET'])
 def add():
-    # item_to_delete = Appointment.query.get_or_404(id)
     
     if request.method == 'POST':
         pxid = request.form['pxid']
@@ -95,32 +78,75 @@ def add():
     else:
         return render_template('add.html')
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    item_to_delete = Appointment.query.get_or_404(id)
+@app.route('/delete/<string:apptid>')
+def delete(apptid):
     
     try:
-        db.session.delete(item_to_delete)
-        db.session.commit()
+        cur = db.connection.cursor()
+        cur.execute(f"DELETE FROM appointments WHERE apptid='{apptid}'")
+        cur.close()
+
+        db.connection.commit()
+
         return redirect('/')
     except:
         return 'Something went wrong with the deletion of that item'
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    item = Appointment.query.get_or_404(id)
+@app.route('/update/<string:upd_id>', methods=['GET', 'POST'])
+def update(upd_id):
+
+    cur = db.connection.cursor()
+    cur.execute(f"SELECT * FROM appointments WHERE apptid = '{upd_id}'")
+    appt_to_update = cur.fetchall()
+    cur.close()
 
     if request.method == 'POST':
-        item.title = request.form['content']
+        pxid = request.form['pxid']
+        clinicid = request.form['clinicid']
+        doctorid = request.form['doctorid']
+        apptid = request.form['apptid']
+        status = request.form['status']
+        timequeued = request.form['timequeued']
+        queuedate = request.form['queuedate']
+        starttime = request.form['starttime']
+        endtime = request.form['endtime']
+        type = request.form['type']
+        virtual = request.form['virtual']
+        hospitalname = request.form['hospitalname']
+        ishospital = request.form['ishospital']
+        city = request.form['city']
+        province = request.form['province']
+        regionname = request.form['regionname']
+        mainspecialty = request.form['mainspecialty']
+        doctor_age = request.form['doctor_age']
+        px_age = request.form['px_age']
+        gender = request.form['gender']
 
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'Something went wrong with updating that item'
+        timequeued = format_time(timequeued)
+        queuedate = format_time(queuedate)
+        starttime = format_time(starttime)
+        endtime = format_time(endtime)
+
+        cur = db.connection.cursor()
+        cur.execute(f"UPDATE appointments\
+                    SET pxid='{pxid}',clinicid='{clinicid}',doctorid='{doctorid}',apptid='{apptid}',status='{status}',\
+                        timequeued='{timequeued}',queuedate='{queuedate}',starttime='{starttime}',endtime='{endtime}',\
+                        type='{type}',`virtual`='{virtual}',hospitalname='{hospitalname}',ishospital='{ishospital}',\
+                        city='{city}',province='{province}',regionname='{regionname}',mainspecialty='{mainspecialty}',\
+                        doctor_age={doctor_age},px_age={px_age},gender='{gender}'\
+                    WHERE apptid='{upd_id}'")
+        cur.close()
+
+        db.connection.commit()
+        return redirect('/')
+
+        # try:
+        
+        # except:
+        #     return 'Something went wrong with updating that item'
         
     else:
-        return render_template('update.html', item = item)
+        return render_template('update.html', item = appt_to_update[0])
 
 if __name__ == "__main__":
     app.run(debug=True)
